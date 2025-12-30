61d006aa744962bd679337c745ae30379e43b82c
Son <thswjddn0812@gmail.com>
2025-12-30 12:19:40 +0900

    cors 설정 후

--- jikgumate_back/src/main.ts
+++ jikgumate_back/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { corsConfig } from './configs/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsConfig);
   // --- 스웨거 설정 시작 ---
   const config = new DocumentBuilder()
     .setTitle('JikguMate API')
